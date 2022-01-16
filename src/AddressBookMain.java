cedf6694e730955fad7aa2d58d48624ee5f75f4b
Pravin Makode <pkumarmakode@lululemon.com>
2022-01-16 11:37:31 +0530

    UC1 Changes to create a contact in AddressBook

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,5 +1,17 @@
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+
+        Contact contact = new Contact();
+        contact.setFirstName("Pravin");
+        contact.setLastName("Makode");
+        contact.setAddress("Address 123");
+        contact.setCity("Bangalore");
+        contact.setState("Karnataka");
+        contact.setEmailId("pravin.kumar.makode@gmail.com");
+        contact.setPhoneNumber("9999999999");
+        contact.setZipCode("123456");
+
+        System.out.println(contact);
     }
 }
